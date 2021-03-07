6a7a706598a09fc23739ab246ce31d139f4006f3
Tomas Gurcys <gurcys@hotmail.co.uk>
2021-03-07 12:24:05 +0200

    Add project files.

---
+++ MunicipalityTaxesManager.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MunicipalityTaxesManager.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MunicipalityTaxesManager.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void WeatherForecastControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
